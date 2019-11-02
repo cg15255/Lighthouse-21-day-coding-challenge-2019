3d018121528646706f6b2b7b75819c092bf7da9f
Chris George <chrisgeorge.9@gmail.com>
2019-11-01 17:55:08 -0700

    Completed day 1 challenge

---
+++ index.js
@@ -0,0 +1,6 @@
+// DAY 1
+const doorToDoor = (volunteers, neighbourhoods) => {
+  const numVolunteers = volunteers.length
+  const numNeighbourhoods = neighbourhoods.length
+  return numNeighbourhoods / numVolunteers
+}
