8d47401795d53bd1745ff95409140d0ead336dce
Chris George <chrisgeorge.9@gmail.com>
2019-11-03 00:06:39 -0700

    Completed day 2 challenge

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ const doorToDoor = (volunteers, neighbourhoods) => {
   const numNeighbourhoods = neighbourhoods.length
   return numNeighbourhoods / numVolunteers
 }
+
+const interviewAnswer = topic => {
+  // Transform text to lower case to account for capital letters in function call.
+  const topicTransformed = topic.toLowerCase()
+  if (topicTransformed === "arts funding") {
+    return "We'll have to get creative!"
+  } else if (topicTransformed === "economy") {
+    return "Time is money."
+  } else if (topicTransformed === "transportation") {
+    return "It's going to be a long road, so we better get moving."
+  } else {
+    return "QUACK!"
+  }
+}
