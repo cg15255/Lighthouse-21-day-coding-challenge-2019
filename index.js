003a39d3e42de6d5973376c52ac7daad2b49e670
Chris George <chrisgeorge.9@gmail.com>
2019-11-09 12:07:17 -0800

    Remove console logs

--- index.js
+++ index.js
@@ -111,23 +111,6 @@ const smartGarbage = (trash, bins) => {
 
 // DAY 9
 
-const cars = [
-  {
-    time: 1568329654807,
-    speed: 40
-  },
-  {
-    time: 1568329821632,
-    speed: 42
-  },
-  {
-    time: 1568331115463,
-    speed: 35
-  }
-]
-
-const speed = 38
-
 const carPassing = (cars, speed) => {
   const newCars = [
     ...cars,
@@ -138,6 +121,3 @@ const carPassing = (cars, speed) => {
   ]
   return newCars
 }
-
-const result = carPassing(cars, speed)
-console.log(result)
