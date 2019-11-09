83f3e4e56e7c76aa9cc57600937603731e85ad14
Chris George <chrisgeorge.9@gmail.com>
2019-11-09 12:08:35 -0800

    clean up solutions

--- index.js
+++ index.js
@@ -40,12 +40,6 @@ const registerToVote = (name, unregisteredVoters) => {
   return filteredArray
 }
 
-const stations = [
-  ["Big Bear Donair", 10, "restaurant"],
-  ["Bright Lights Elementary", 50, "school"],
-  ["Moose Mountain Community Centre", 45, "community centre"]
-]
-
 // DAY 5
 
 const chooseStations = stations => {
